2b8100069d201e161f46a07eb1c2ca9194557bfc
Stjepan Belosa <belosa.fip@gmail.com>
2026-03-22 15:55:13 +0100

    Brisanje višaka

--- app/themes/altum/views/sitemap/sitemap_main.php
+++
@@ -1,9 +0,0 @@
-<?php defined('ALTUMCODE') || die() ?>
-<?= '<?xml version="1.0" encoding="UTF-8"?>' ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <?php foreach ($data->sitemap_urls as $url) : ?>
-        <url>
-            <loc><?= $url ?></loc>
-        </url>
-    <?php endforeach ?>
-</urlset>
